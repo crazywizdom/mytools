13bd59aeb03e09f66d8c3b249e84a070d659fa52
Maneesha Jain <maneesha.jain@gmail.com>
2018-01-09 22:06:49 -0800

    Update PersonRunnerTest.java

--- src/test/java/edu/ppu/runners/PersonRunnerTest.java
+++ src/test/java/edu/ppu/runners/PersonRunnerTest.java
@@ -16,6 +16,7 @@ public class PersonRunnerTest {
 	public void testGetAgeCategory() {
 		PersonRunner cut = new PersonRunner(null);
 		int ageCategory = cut.getAgeCategory(70);
+		
 		assertEquals("wrong age category", 3, ageCategory);
 	}
 
